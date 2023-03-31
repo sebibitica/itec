8c473913b55569924a8cf0ddd94ad26fd9e7dd15
Calin <bordeanu.calin05@gmail.com>
2023-03-31 22:22:43 +0300

    added firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDa3LYk813ZXQVYP_wErlQYnlT_2r42xdk",
+  authDomain: "itec-33d26.firebaseapp.com",
+  projectId: "itec-33d26",
+  storageBucket: "itec-33d26.appspot.com",
+  messagingSenderId: "1099159251567",
+  appId: "1:1099159251567:web:21593d8d2e539fc09f3b9a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
