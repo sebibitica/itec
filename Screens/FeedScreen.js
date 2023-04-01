0b3580341e452a4ff52dfbf4f574c5a5a672ec02
Sebastian Bitica <biticasebi@gmail.com>
2023-04-01 20:28:33 +0300

    api working

--- Screens/FeedScreen.js
+++ Screens/FeedScreen.js
@@ -1,59 +1,87 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import EventPill from '../components/FeedComp';
-import data from '../events.json'
-import { Text } from 'react-native';
-import { FlatList } from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context'
+import React from "react";
+import { StyleSheet, Button, View } from "react-native";
+import EventPill from "../components/FeedComp";
+import data from "../events.json";
+import { Text } from "react-native";
+import { FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/core";
+import { useState, useEffect } from "react";
+
+const dateToday = new Date().toISOString().slice(0, 10);
+console.log(dateToday);
+
+const url =
+  "https://b2bw.fluxer.io/rest/script_php_306/getEvenimenteZileNopti/"+dateToday;
 
 const FeedScreen = () => {
+  const navigation = useNavigation();
+  const [data1, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
 
-    const renderItem = ({item}) => (
-        <EventPill
-        key={item.id}
-        name={item.name}
-        category={item.category}
-        description={item.description}
-        program={item.program}
-        location={item.location}
-        date={item.date}
+  console.log(data1);
+
+  const nowH = new Date().getHours(),
+    nowM = new Date().getMinutes();
+  const now = nowH + ":" + nowM;
+
+  const filteredData = data1.filter((item) => {
+    const eventTime = item.ora;
+    return eventTime >= now;
+  });
+
+  const renderItem = ({ item }) => (
+    <EventPill
+      name={item.titlu}
+      category={item.tip}
+      description={item.descriere}
+      program={item.ora}
+      location={item.adresa}
+    />
+  );
+
+  return (
+    <View style={{ height: "100%" }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Button
+          title="press"
+          onPress={() => navigation.navigate("QRScanner")}
         />
-    )
-
-    return (
-        <View style={{height:"100%"}}>
-        <SafeAreaView style={{flex:1}} >
-        <Text style = {styles.title}>Events today</Text>
+        <Text style={styles.title}>Events today</Text>
         <FlatList
-        data={data.events}
-        renderItem={renderItem}
-        keyExtractor={item => item.id.toString()}
-        style={{flexGrow:1, margin:10}}
+          data={filteredData}
+          renderItem={renderItem}
+          style={{ flexGrow: 1, margin: 10 }}
         />
-        </SafeAreaView>
-        </View>
-    );
-  };
+      </SafeAreaView>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    title: {
-        fontSize: 32,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginTop: 200,
-        marginBottom: 20,
-        color: '#333',
-        textShadowColor: '#ccc',
-        textShadowOffset: { width: 2, height: 2 },
-        textShadowRadius: 2,
-      },
-      scrollViewContent: {
-        minHeight: '100%',
-        padding: 10,
-        marginBottom:100,
-      },
-
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 200,
+    marginBottom: 20,
+    color: "#333",
+    textShadowColor: "#ccc",
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 2,
+  },
+  scrollViewContent: {
+    minHeight: "100%",
+    padding: 10,
+    marginBottom: 100,
+  },
 });
 
 export default FeedScreen;
-
--- components/FeedComp.js
+++ components/FeedComp.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { TouchableOpacity } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/core";
 
-const EventPill = ({ name, category, description, program, location, date }) => {
+const EventPill = ({ name, category, description, program, location }) => {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <View style={styles.iconContainer}>
@@ -11,27 +13,38 @@ const EventPill = ({ name, category, description, program, location, date }) => 
       </View>
       <View style={styles.infoContainer}>
         <Text style={styles.name}>{name}</Text>
-        <View style = {{flexDirection: 'row',}}>
-            <Text style={styles.category}>{category}</Text>
-            <Text style={styles.category}>   -   </Text>
-            <Text style={styles.category}>{date}</Text>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={styles.category}>{category}</Text>
+          <Text style={styles.category}> - </Text>
         </View>
-        
+
         <Text style={styles.description}>{description}</Text>
         <View style={styles.programLocationContainer}>
-          <MaterialCommunityIcons name="clock-outline" size={16} color="white" style={styles.icon} />
-          <Text  style={styles.program}>{program}</Text>
-          <MaterialCommunityIcons name="map-marker-outline" size={16} color="white" style={styles.icon} />
-          <Text numberOfLines={1} ellipsizeMode='tail' style={styles.location}>{location}</Text>
+          <MaterialCommunityIcons
+            name="clock-outline"
+            size={16}
+            color="white"
+            style={styles.icon}
+          />
+          <Text style={styles.program}>{program}</Text>
+          <MaterialCommunityIcons
+            name="map-marker-outline"
+            size={16}
+            color="white"
+            style={styles.icon}
+          />
+          <Text numberOfLines={1} ellipsizeMode="tail" style={styles.location}>
+            {location}
+          </Text>
         </View>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={[styles.button, styles.leftButton]}>
+          <TouchableOpacity
+            style={[styles.button, styles.leftButton]}
+            //onPress={}
+          >
             <Text style={styles.buttonText}>About</Text>
           </TouchableOpacity>
         </View>
-
-        
-
       </View>
     </View>
   );
@@ -39,51 +52,50 @@ const EventPill = ({ name, category, description, program, location, date }) => 
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#4A4A4A',
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#4A4A4A",
     borderRadius: 20,
     padding: 19,
     marginVertical: 10,
   },
   iconContainer: {
-    backgroundColor: '#EDC71C',
+    backgroundColor: "#EDC71C",
     borderRadius: 20,
     padding: 10,
     marginRight: 10,
-    
   },
   infoContainer: {
     flex: 1,
   },
   name: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
     fontSize: 20,
     marginBottom: 5,
   },
   category: {
-    color: '#8F8F8F',
+    color: "#8F8F8F",
     fontSize: 14,
     marginBottom: 5,
   },
   description: {
-    color: 'white',
+    color: "white",
     fontSize: 14,
     marginBottom: 5,
   },
   programLocationContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   program: {
-    color: 'white',
+    color: "white",
     fontSize: 14,
     marginLeft: 5,
     marginRight: 20,
   },
   location: {
-    color: 'white',
+    color: "white",
     fontSize: 12,
   },
   icon: {
@@ -91,14 +103,14 @@ const styles = StyleSheet.create({
     marginRight: 5,
   },
   buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
-    backgroundColor: '#4A4A4A',
+    backgroundColor: "#4A4A4A",
     paddingHorizontal: 19,
     paddingVertical: 10,
-    marginTop:14,
+    marginTop: 14,
   },
   button: {
     borderRadius: 20,
@@ -106,18 +118,17 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   leftButton: {
-    backgroundColor: '#EDC71C',
+    backgroundColor: "#EDC71C",
     borderBottomLeftRadius: 15,
-    width:"100%",
-
+    width: "100%",
   },
   rightButton: {
-    backgroundColor: '#8F8F8F',
+    backgroundColor: "#8F8F8F",
     borderBottomRightRadius: 15,
   },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
     fontSize: 16,
   },
 });
