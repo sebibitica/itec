6b38cc24474d4a5da75152d7cea1e088c3a8ee3b
Tudor Todea <tudortodea@gmail.com>
2023-04-02 00:23:54 +0300

    comitul meu

---
+++ Screens/EventPage.js
@@ -0,0 +1,116 @@
+import { useRoute } from '@react-navigation/native';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+export function EventPage ({ name, category, description, program, location }){
+
+    const route = useRoute();
+    return (
+        <SafeAreaView style={styles.safeArea}>
+            <View style={styles.container}>
+                <View>
+                <View style={styles.header} className>
+                    <Text style={styles.title}>{route.params.name}</Text>
+                    <View style={styles.categoryContainer}>
+                        <Text style={styles.category}>{route.params.category}</Text>
+                        <Text style={styles.divider}>|</Text>
+                        <Text style={styles.category}>{route.params.program}</Text>
+                    </View>
+                </View>
+                
+
+                <View style={styles.content}>
+                    <Text style={styles.description}>{route.params.description}</Text>
+                    <View style={styles.locationContainer}>
+                        <Text style={styles.location}>{route.params.location}</Text>
+                    </View>
+                </View>
+                </View>
+                <View style={styles.buttonsContainer}>
+                    <TouchableOpacity style={[styles.button, styles.aboutButton]}>
+                        <Text style={styles.buttonText}>I'm interested</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.button, styles.mapsButton]}>
+                        <Text style={styles.buttonText}>Maps</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'space-between',
+    },
+    header: {
+        padding: 20,
+        backgroundColor: '#4A4A4A',
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginBottom: 10,
+    },
+    categoryContainer: {
+        flexDirection: 'row',
+    },
+    category: {
+        color: '#EDC71C',
+        fontSize: 16,
+        marginRight: 10,
+    },
+    divider: {
+        color: '#EDC71C',
+        fontSize: 16,
+        marginHorizontal: 5,
+    },
+    content: {
+        padding: 20,
+    },
+    description: {
+        fontSize: 18,
+        marginBottom: 20,
+        lineHeight: 28,
+        color: '#4A4A4A',
+    },
+    locationContainer: {
+        borderTopWidth: 1,
+        borderTopColor: '#ccc',
+        paddingTop: 20,
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 20,
+        paddingBottom: 30,
+    },
+    button: {
+        borderRadius: 10,
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '48%',
+    },
+    aboutButton: {
+        backgroundColor: '#4A4A4A',
+    },
+    mapsButton: {
+        backgroundColor: '#EDC71C',
+        
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+    },
+});
